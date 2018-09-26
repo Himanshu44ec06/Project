1ec9ed31c12d41e57228c408523eda3eaea8aae2
unknown <himanshu.44ec06@gmail.com>
2018-09-26 23:46:59 +0530

    first commit

---
+++ Server/init/database.js
@@ -0,0 +1,11 @@
+const  mongoose  = require('mongoose');
+
+module.exports = {
+    start :  function(connectionstring){
+        mongoose.connect(connectionstring);
+        mongoose.connection
+            .once('open',()=>{console.log('connected with DB');})
+            .on('error',(err)=>{ console.warn('ERR => ', err)});
+    }
+
+}
---
+++ Server/server.js
@@ -0,0 +1,7 @@
+const env =  require('./config/config'),
+database  =  require('./init/database');
+
+
+
+// Intializing Database
+database.start();
